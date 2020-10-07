5ec67b669dbd73fbfe2603bba5247cd9c0b6d704
ancoulon <ancoulon@student.s19.be>
2020-10-07 09:46:59 +0200

    updated daemon and install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -33,14 +33,17 @@ execute "mkdir" "-p" "$HOME/.42"
 # clone dot42 inside the directory
 execute "git" "clone" "https://github.com/30c27b/dot42" "$HOME/.42"
 
-# launch dot42 setup command
-execute "$HOME/.42/bin/dot42" "setup"
-
 # give execute permission to shell scripts
 execute "chmod" "-R" "+x" "$HOME/.42/scripts"
 
-# install dot42 daemon
-execute "cp" "$HOME/.42/daemon/dot42.plist" "$HOME/Library/LaunchAgents/com.30c27b.dot42.plist"
+# create log directory
+execute "mkdir" "$HOME/.42/log"
+
+# install dot42 daemon and replace replace HOMEDIR with the user's home directory in the launchagent
+execute "sed" "-e" "s/HOMEDIR/$HOME/g" "$HOME/.42/daemon/dot42.plist" ">" "$HOME/Library/LaunchAgents/com.30c27b.dot42.plist"
+
+# launch dot42 setup command
+#execute "$HOME/.42/bin/dot42" "setup"
 
 # done
 execute "echo" "dot42 is now installed."
